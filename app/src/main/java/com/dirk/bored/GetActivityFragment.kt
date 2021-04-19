57cc153790344e12b515f23216d27c87d449d439
Dirk <64075699+DirkVolkmann@users.noreply.github.com>
2021-04-20 01:10:05 +0200

    settings actually do something now

--- app/src/main/java/com/dirk/bored/GetActivityFragment.kt
+++ app/src/main/java/com/dirk/bored/GetActivityFragment.kt
@@ -1,6 +1,7 @@
 package com.dirk.bored
 
 import android.os.Bundle
+import android.util.ArrayMap
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,9 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.preference.PreferenceManager
 import com.android.volley.Request
+import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
@@ -35,24 +36,24 @@ class GetActivityFragment : Fragment() {
 
         // Read from settings
         val sharedPref = PreferenceManager.getDefaultSharedPreferences(activity)
-        val switchTypePref =            sharedPref.getBoolean("switch_type", false)
-        val switchParticipantsPref =    sharedPref.getBoolean("switch_participants", false)
-        val switchPricePref =           sharedPref.getBoolean("switch_price", false)
-        val switchAccessibilityPref =   sharedPref.getBoolean("switch_accessibility", false)
+        val prefTypeSwitch =            sharedPref.getBoolean("switch_type", false)
+        val prefParticipantsSwitch =    sharedPref.getBoolean("switch_participants", false)
+        val prefPriceSwitch =           sharedPref.getBoolean("switch_price", false)
+        val prefAccessibilitySwitch =   sharedPref.getBoolean("switch_accessibility", false)
 
-        val typePref = sharedPref.getString("pref_type", getString(string.summary_not_set))
-        val participantsPref = sharedPref.getString("pref_participants", getString(string.summary_not_set))
-        val pricePref = sharedPref.getInt("pref_price", 50)
-        val accessibilityPref = sharedPref.getInt("pref_accessibility", 50)
+        val prefType =          sharedPref.getString("pref_type", getString(string.summary_not_set))
+        val prefParticipants =  sharedPref.getString("pref_participants", getString(string.summary_not_set))
+        val prefPrice =         sharedPref.getInt("pref_price", 50)
+        val prefAccessibility = sharedPref.getInt("pref_accessibility", 50)
 
-        Log.d("READ_SETTINGS", "switchTypePref $switchTypePref")
-        Log.d("READ_SETTINGS", "switchParticipantsPref $switchParticipantsPref")
-        Log.d("READ_SETTINGS", "switchPricePref $switchPricePref")
-        Log.d("READ_SETTINGS", "switchAccessibilityPref $switchAccessibilityPref")
-        Log.d("READ_SETTINGS", "typePref " + typePref.toString())
-        Log.d("READ_SETTINGS", "participantsPref $participantsPref")
-        Log.d("READ_SETTINGS", "pricePref $pricePref")
-        Log.d("READ_SETTINGS", "accessibilityPref $accessibilityPref")
+        Log.d("GetActivity", "switchTypePref $prefTypeSwitch")
+        Log.d("GetActivity", "switchParticipantsPref $prefParticipantsSwitch")
+        Log.d("GetActivity", "switchPricePref $prefPriceSwitch")
+        Log.d("GetActivity", "switchAccessibilityPref $prefAccessibilitySwitch")
+        Log.d("GetActivity", "typePref $prefType")
+        Log.d("GetActivity", "participantsPref $prefParticipants")
+        Log.d("GetActivity", "pricePref $prefPrice")
+        Log.d("GetActivity", "accessibilityPref $prefAccessibility")
 
         // Instantiate view variables
         val button = view.findViewById<Button>(R.id.button_first)
@@ -61,18 +62,37 @@ class GetActivityFragment : Fragment() {
         // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this.context)
         val url = "https://www.boredapi.com/api/activity/"
-        val params = null
         val tag = "GetActivity"
+
+        // Build params
+        var paramsMap = ArrayMap<String, String>()
+        if (!prefTypeSwitch) {
+            paramsMap.put("type", prefType)
+        }
+        if (!prefParticipantsSwitch) {
+            paramsMap.put("participants", prefParticipants)
+        }
+        if (!prefPriceSwitch) {
+            val paramPrice = (prefPrice.toFloat()/100).toString()
+            paramsMap.put("price", paramPrice)
+        }
+        if (!prefAccessibilitySwitch) {
+            val paramAccessibility = (prefAccessibility.toFloat()/100).toString()
+            paramsMap.put("accessibility", paramAccessibility)
+        }
+
+        val paramsString = paramsMap.entries.map { (it.key + "=" +  it.value) }.joinToString(prefix = "?", separator = "&")
+
+        Log.d("GetActivity", paramsString)
 
         button.setOnClickListener {
             //findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
             // Request a string response from the provided URL.
-            val jsonObjectRequest = JsonObjectRequest(
-                Request.Method.GET, url, params,
+            val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url + paramsString, null,
                 { response ->
                     // Display the first 500 characters of the response string.
                     Log.d("GetActivity", response.toString())
-                    var activityText: String = "Failed to load activity"
+                    var activityText: String = getString(string.request_failed)
                     if (response.has("activity")) {
                         activityText = response.getString("activity")
                         button.text = getString(string.get_another_activity)
@@ -80,8 +100,8 @@ class GetActivityFragment : Fragment() {
                     textView.text = activityText
                 },
                 {
-                    Log.d("GetActivity", "Failed to load activity")
-                    textView.text = "Failed to load activity"
+                    textView.text = getString(string.request_failed)
+                    Log.d("GetActivity", getString(string.request_failed))
                 }
             )
 
